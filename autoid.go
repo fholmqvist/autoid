91f12d25ab9b7696c4eb49b172e4636ff01bb158
Holmqvist1990 <fredrik.d.holmqvist@gmail.com>
2022-06-21 23:10:24 +0200

    feature: Set(id), refactor: use Set(id), refactor: rename global id to __id to stand out

--- autoid.go
+++ autoid.go
@@ -6,50 +6,54 @@ import (
 )
 
 var (
-	id          uint64 = 0
+	__id        uint64 = 0
 	errOverflow string = "overflow for %v: %d"
 )
-
-func Reset() {
-	id = 0
-}
 
 func Next() (uint32, error) {
 	return NextUint32()
 }
 
+func Set(id uint64) {
+	__id = id
+}
+
+func Reset() {
+	__id = 0
+}
+
 func NextUint8() (uint8, error) {
-	n := next()
-	if n > math.MaxUint8 {
-		return 0, fmt.Errorf(errOverflow, "uint8", n)
+	id := next()
+	if id > math.MaxUint8 {
+		return 0, fmt.Errorf(errOverflow, "uint8", id)
 	}
-	return uint8(n), nil
+	return uint8(id), nil
 }
 
 func NextUint16() (uint16, error) {
-	n := next()
-	if n > math.MaxUint16 {
-		return 0, fmt.Errorf(errOverflow, "uint16", n)
+	id := next()
+	if id > math.MaxUint16 {
+		return 0, fmt.Errorf(errOverflow, "uint16", id)
 	}
-	return uint16(n), nil
+	return uint16(id), nil
 }
 
 func NextUint32() (uint32, error) {
-	n := next()
-	if n > math.MaxUint32 {
-		return 0, fmt.Errorf(errOverflow, "uint32", n)
+	id := next()
+	if id > math.MaxUint32 {
+		return 0, fmt.Errorf(errOverflow, "uint32", id)
 	}
-	return uint32(n), nil
+	return uint32(id), nil
 }
 
 func NextUint64() (uint64, error) {
-	if id == math.MaxUint64 {
-		return 0, fmt.Errorf(errOverflow, "uint32", id)
+	if __id == math.MaxUint64 {
+		return 0, fmt.Errorf(errOverflow, "uint32", __id)
 	}
 	return next(), nil
 }
 
 func next() uint64 {
-	id++
-	return id
+	__id++
+	return __id
 }
--- autoid_test.go
+++ autoid_test.go
@@ -7,43 +7,43 @@ import (
 
 func Test8(t *testing.T) {
 	defer Reset()
-	if n, err := NextUint8(); n != 1 || err != nil {
-		t.Fatalf("expected (1, <nil>), got (%d, %v)", n, err)
+	if id, err := NextUint8(); id != 1 || err != nil {
+		t.Fatalf("expected (1, <nil>), got (%d, %v)", id, err)
 	}
-	id = math.MaxUint8
+	Set(math.MaxUint8)
 	if _, err := NextUint8(); err == nil {
-		t.Fatalf("expected overflow, got %d", id)
+		t.Fatalf("expected overflow, __id was %d", __id)
 	}
 }
 
 func Test16(t *testing.T) {
 	defer Reset()
-	if n, err := NextUint16(); n != 1 || err != nil {
-		t.Fatalf("expected (1, <nil>), got (%d, %v)", n, err)
+	if id, err := NextUint16(); id != 1 || err != nil {
+		t.Fatalf("expected (1, <nil>), got (%d, %v)", id, err)
 	}
-	id = math.MaxUint16
+	Set(math.MaxUint16)
 	if _, err := NextUint16(); err == nil {
-		t.Fatalf("expected overflow, got %d", id)
+		t.Fatalf("expected overflow, __id was %d", __id)
 	}
 }
 func Test32(t *testing.T) {
 	defer Reset()
-	if n, err := NextUint32(); n != 1 || err != nil {
-		t.Fatalf("expected (1, <nil>), got (%d, %v)", n, err)
+	if id, err := NextUint32(); id != 1 || err != nil {
+		t.Fatalf("expected (1, <nil>), got (%d, %v)", id, err)
 	}
-	id = math.MaxUint32
+	Set(math.MaxUint32)
 	if _, err := NextUint32(); err == nil {
-		t.Fatalf("expected overflow, got %d", id)
+		t.Fatalf("expected overflow, __id was %d", __id)
 	}
 }
 
 func Test64(t *testing.T) {
 	defer Reset()
-	if n, err := NextUint64(); n != 1 || err != nil {
-		t.Fatalf("expected (1, <nil>), got (%d, %v)", n, err)
+	if id, err := NextUint64(); id != 1 || err != nil {
+		t.Fatalf("expected (1, <nil>), got (%d, %v)", id, err)
 	}
-	id = math.MaxUint64
+	Set(math.MaxUint64)
 	if _, err := NextUint64(); err == nil {
-		t.Fatalf("expected overflow, got %d", id)
+		t.Fatalf("expected overflow, __id was %d", __id)
 	}
 }
